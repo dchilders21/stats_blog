e4490f6abe19bd8803e59190f52156ddff810d58
dchilders21 <dchilders21@gmail.com>
2017-01-02 18:05:26 -0800

    Updated Views.py to fix the internal server error with the API.  Also added the create_posts.py which will automate the blog creation.

--- stats_blog/create_posts.py
+++ stats_blog/create_posts.py
@@ -1,19 +1,33 @@
-print('Trying to create a post')
 import requests
+import urlparse
+import subprocess
+import warnings
 
-url = "http://127.0.0.1:8000/api/posts"
-#client = requests.session()
-#client.get(url)
-csrftoken = requests.get(url).cookies['csrftoken']
-#csrftoken = client.cookies['csrftoken']
-#csrftoken = 'DK0b6yGb05PiIFJhPpzGSgV6kXhtaiiFXPKvUdHYCXcyjtlafJYudEGvLsx8bHaL'
-print(csrftoken)
-header = {'X-CSRFToken': csrftoken}
-cookies = {'csrftoken': csrftoken}
-payload = {
-    'csrfmiddlewaretoken': csrftoken,
-    'title': 'test_title',
-    'content': 'test_content'
-}
+CLIENT_ID = "ZDIYrYeuu09pWRgDaOEsMsteu8u95BzL2BXWc95y"
+CLIENT_SECRET = "UumOO1wjPf88gVA93091dlcobrOhrpFw30HeOHwjvr2Ld1o6poqucHCJAPOp0axqvN5ryHvvDjA5lmVT89UD0kuFfhFVCYQqgv9LYLz9VXiZTojrv0JrVV7TqObmf7yf"
 
-r = requests.post(url, data=payload, headers=header, cookies=cookies)
+USERNAME = 'sammy'
+PASSWORD = '1qaz2wsx'
+
+oauth_args = dict(client_id=CLIENT_ID,
+                  client_secret=CLIENT_SECRET,
+                  grant_type='password',
+                  username=USERNAME,
+                  password=PASSWORD)
+
+req = requests.post('http://localhost:8000/api/oauth2/token/', data=oauth_args)
+print(req.text)
+json = req.json()
+
+if 'access_token' in json:
+  AUTHORIZATION=json['token_type'] + ' ' + json['access_token']
+  print(AUTHORIZATION)
+  params = dict(authorization=AUTHORIZATION)
+  content = dict(title='automated_title',
+                 content='automated_content')
+  r = requests.post('http://127.0.0.1:8000/api/posts?format=json', headers=params, data=content)
+  print(r.text)
+else:
+  print('No Key Given')
+
+
